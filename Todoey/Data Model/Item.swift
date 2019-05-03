355484ca872e479f145f63c85359403dde290634
Alex <abc_emcm@hotmail.com>
2019-05-03 12:23:37 -0500

    Change to MVC pattern design

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alejandro Bonilla on 4/5/19.
+//  Copyright © 2019 Alejandro Bonilla. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var tittle: String = ""
+    var done: Bool = false
+}
